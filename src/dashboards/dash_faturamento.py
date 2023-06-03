dd81c8a93cfe635fd91932e152b39e2508a79428
Ricardo Fernandes <pelais.fernandes@gmail.com>
2023-06-03 12:13:40 -0300

    Adição de cverificação de colunas Decimal

--- src/dashboards/dash_faturamento.py
+++ src/dashboards/dash_faturamento.py
@@ -2,6 +2,7 @@ from connection.conn_firebird import connect_to_firebird, execute_query
 import pandas as pd
 from env import FIREBIRD_HOST, FIREBIRD_USER, FIREBIRD_PASSWORD, FIREBIRD_DATABASE_ATUAL, FIREBIRD_DATABASE_BACKUP
 import requests
+from decimal import Decimal
 
 def execute_firebird_query(query):
     fb_1 = connect_to_firebird(
@@ -24,6 +25,12 @@ def execute_firebird_query(query):
     df1 = pd.DataFrame(result1, columns=columns1)
     df2 = pd.DataFrame(result2, columns=columns2)
     df_combine = pd.concat([df1, df2], axis=0, ignore_index=True)
+
+    # Verificar se a coluna é decimal e aplica o float no dataframe
+    decimal_columns = df_combine.apply(lambda s: s.apply(lambda x: isinstance(x, Decimal)).any())
+    for col in decimal_columns[decimal_columns].index:
+        df_combine[col] = df_combine[col].astype(float)
+        
     df_final = df_combine.drop_duplicates()
 
     return df_final
